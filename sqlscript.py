7fe557fe660a892f632db94e6cd224a0e764834d
Rahul Kumar <rahul.kumar2@mail.mcgill.ca>
2019-07-03 10:42:07 +0400

    Adopted OOP in both modules for encapsulation

--- sqlscript.py
+++ sqlscript.py
@@ -1,47 +1,66 @@
-
 import pymysql.cursors
 import datetime
 import re
 from subprocess import run
-def get_tables(uid):
+class GetSignalData:
+    def __init__(self,object_fullpath):
+        self.result=()
+        self.connection=pymysql.connect(host='localhost',user='mcisadmin',password='s$e!P!C!L@2014',db='pacis')
+        try:
+            with self.connection.cursor() as cursor:
+                self.sql="select * from objects where object_fullpath='{}'".format(object_fullpath)
+                cursor.execute(self.sql)
+                self.result=cursor.fetchall()
+                self.uid=self.result[0][1]
+                if self.result:
+                    run("mysqldump -u mcisadmin -ps$e!P!C!L@2014 pacis > pacis.sql",shell=True)
+                    self.all_tables=self.get_tables(self.uid)
+                    run('del pacis.sql',shell=True)
+                    if self.all_tables:
+                        self.sql="SELECT * FROM "+','.join(self.all_tables)
+                        cursor.execute(self.sql)
+                        self.result = cursor.fetchall()
+                    else: 
+                        self.result = 0
+        except Exception as e:
+            print(e)
+            self.result= -2
+        finally:
+            self.connection.close()
+    def get_tables(self,uid):
+        self.all_tables=[]
+        try:
+            self.file=open("pacis.sql",'r')
+            pattern=re.compile(r"INSERT INTO `values_\d{1,}` VALUES [.\(\),0-9''-:\s]+?,"+str(uid))
+            pattern2=re.compile(r'values_\d{1,}')
+            for line in self.file:
+                matches=pattern.finditer(line)
+                for match in matches:
+                    sub_string=line[match.span()[0]:match.span()[1]]
+                    table=pattern2.findall(sub_string)
+                    self.all_tables.append(table[0])
+        finally:
+            self.file.close()
+            return self.all_tables
 
-    all_tables=[]
-    try:
-        file=open("pacis.sql",'r')
-        pattern=re.compile(r"INSERT INTO `values_\d{1,}` VALUES [.\(\),0-9''-:\s]+?,"+str(uid))
-        pattern2=re.compile(r'values_\d{1,}')
-        for line in file:
-            matches=pattern.finditer(line)
-            for match in matches:
-                sub_string=line[match.span()[0]:match.span()[1]]
-                table=pattern2.findall(sub_string)
-                all_tables.append(table[0])
-    finally:
-        file.close()
-        return all_tables
-def main(object_fullpath):
-    result=()
-    connection=pymysql.connect(host='localhost',user='mcisadmin',password='s$e!P!C!L@2014',db='pacis')
-    try:
-        with connection.cursor() as cursor:
-            sql="select * from objects where object_fullpath='{}'".format(object_fullpath)
-            cursor.execute(sql)
-            result=cursor.fetchall()
-            if result:
-                run("mysqldump -u mcisadmin -ps$e!P!C!L@2014 pacis > pacis.sql",shell=True)
-                all_tables=get_tables(result[0][1])
-                run('del pacis.sql',shell=True)
-                if all_tables:
-                    sql="SELECT * FROM "+','.join(all_tables)
-                    cursor.execute(sql)
-                    return cursor.fetchall()
-                else:
-                    return 0
-    except Exception as e:
-        print(e)
-        return -2
-    finally:
-        connection.close()
+class GetSignals:
+    def __init__(self):
+        self.result=()
+        self.connection=pymysql.connect(host='localhost',user='mcisadmin',password='s$e!P!C!L@2014',db='pacis')
+        try:
+            with self.connection.cursor() as cursor:
+                self.sql="SELECT object_fullpath FROM objects"
+                cursor.execute(self.sql)
+                cursor.fetchmany()
+                self.result=cursor.fetchall()
+        except Exception as e:
+            print(e)
+            self.result=-2
+        finally:
+            self.connection.close()
+
+
 if __name__ == "__main__":
-    main('MOSG / 11KV / K05_T40 LV INC / MEASUREMENT / VOLTAGE VYN')
+    data=GetSignalData('MOSG / 11KV / K05_T40 LV INC / MEASUREMENT / VOLTAGE VYN')
+    print(data.result[0])
 
--- xlsscript.py
+++ xlsscript.py
@@ -1,79 +1,85 @@
 import xlsxwriter
 import datetime
-import sqlscript
-def roundTime(d1,roundTo):
-    seconds=(d1-d1.min).seconds
-    rounding=(seconds+roundTo/2)//roundTo*roundTo
-    d2=d1+datetime.timedelta(0,rounding-seconds,-d1.microsecond)
-    if(d2>d1):
-        return d2
-    else:
-        return d2+datetime.timedelta(seconds=roundTo)
-def addSecs(tm,ms):
-    d1=datetime.datetime(1,1,1,tm.hour,tm.minute,tm.second)
-    d1+=datetime.timedelta(milliseconds=ms)
-    return d1.time()
-def main(fdate,tdate,fq,radb,hidden,object_fullpath):
-    try:    
-        workbook = xlsxwriter.Workbook('./SignalLog/tables.xlsx')
-        worksheet = workbook.add_worksheet()
-        cell_format=workbook.add_format({'font_color':'white','bg_color':'#107896',\
-    'font_name':'Times New Roman','bold':True,'underline':True,'center_across':True,'font_size':20})  
-        caption = 'LOGS'
-        worksheet.set_column('B:G', 20)
-        worksheet.write('D1', caption,cell_format)
-        unparsed_data=sqlscript.main(object_fullpath)
-        if not unparsed_data:
-            workbook.close()
-            return -3
-        data=[]
-    
-        if not hidden:
-            option={1:1.0,2:60.0,3:3600.0}
-            roundTo=(float)(fq)*option.get(radb)
-            flag={}
-            if unparsed_data:
-                flag[unparsed_data[0][1]]=0
-            for vm,vo,dt,ms,vq,vv in unparsed_data:
-                dt=dt+datetime.timedelta(milliseconds=ms)
-                if dt>tdate:
-                    if flag[vo]==1:
-                        flag[vo]=0
-                    continue
-                if dt<fdate :
-                    continue
-                elif dt >=fdate and dt < (tdate+datetime.timedelta(1)) and flag[vo]==0:
-                    flag[vo]=1
-                    cur_val=(vm,vo,vq,vv)
-                    next_dt=roundTime(dt,roundTo)
-                    data.append((str(dt.date()),str(addSecs(dt.time(),ms)),vm,vo,vq,vv))
-                    continue
-                elif flag[vo]==1 and dt<=next_dt:
-                    cur_val=(vm,vo,vq,vv)
-                elif flag[vo]==1 and dt>next_dt:
-                    while dt>next_dt and next_dt<tdate:
-                        data.append((str(next_dt.date()),str(next_dt.time()),*cur_val))
-                        next_dt=roundTime(next_dt,roundTo)
-                    cur_val=(vm,vo,vq,vv)
+from sqlscript import GetSignalData
+
+class ParseData:
+    def __init__(self,fdate,tdate,fq,radb,hidden,object_fullpath):
+        try:    
+            self.workbook = xlsxwriter.Workbook('./SignalLog/tables.xlsx')
+            self.worksheet = self.workbook.add_worksheet()
+            cell_format=self.workbook.add_format({'font_color':'white','bg_color':'#107896',\
+        'font_name':'Times New Roman','bold':True,'underline':True,'center_across':True,'font_size':20})  
+            caption = 'LOGS'
+            self.worksheet.set_column('B:G', 20)
+            self.worksheet.write('D1', caption,cell_format)
+            self.signaldata=GetSignalData(object_fullpath)
+            self.unparsed_data=self.signaldata.result
+            self.uid=self.signaldata.uid
+            if not self.unparsed_data:
+                self.workbook.close()
+                self.result= -3
+            else:
+                self.data=[]
+                if not hidden:
+                    self.option={1:1.0,2:60.0,3:3600.0}
+                    self.roundTo=(float)(fq)*self.option.get(radb)
+                    self.flag={}
+                    if self.unparsed_data:
+                        self.flag[self.uid]=0
+                    for vm,vo,dt,ms,vq,vv in self.unparsed_data:
+                        dt=dt+datetime.timedelta(milliseconds=ms)
+                        if dt<fdate or vo !=self.uid:
+                            continue
+                        if dt>tdate:
+                            if self.flag[vo]==1:
+                                self.flag[vo]=0
+                            continue
+                        elif dt >=fdate and dt < (tdate+datetime.timedelta(1)) and self.flag[vo]==0:
+                            self.flag[vo]=1
+                            cur_val=(vm,vo,vq,vv)
+                            next_dt=self.roundTime(dt,self.roundTo)
+                            self.data.append((str(dt.date()),str(self.addSecs(dt.time(),ms)),vm,vo,vq,vv))
+                            continue
+                        elif self.flag[vo]==1 and dt<=next_dt:
+                            cur_val=(vm,vo,vq,vv)
+                        elif self.flag[vo]==1 and dt>next_dt:
+                            while dt>next_dt and next_dt<tdate:
+                                self.data.append((str(next_dt.date()),str(next_dt.time()),*cur_val))
+                                next_dt=self.roundTime(next_dt,self.roundTo)
+                            cur_val=(vm,vo,vq,vv)
+                else:
+                    for vm,vo,dt,ms,vq,vv in self.unparsed_data:
+                        if(dt>=fdate and dt<(tdate+datetime.timedelta(1)) and  vo==self.uid):
+                            self.data.append((str(dt.date()),str(self.addSecs(dt.time(),ms)),vm,vo,vq,vv))
+                if self.data:
+                    options = {'banded_rows': 0, 'banded_columns': 1,'data': self.data,
+                        'columns': [{'header':'Date'},
+                                    {'header': 'Time'},
+                                    {'header': 'Value Meantype'},
+                                    {'header': 'Value Object UID '},
+                                    {'header': 'Value Quality'},
+                                    {'header': 'Value Value'},
+                                    ]} 
+                    self.worksheet.add_table('B3:G'+str(3+len(self.data)),options)
+                self.workbook.close()
+                self.result = int(not self.data==[])
+        except PermissionError:
+            self.result = -1
+    def roundTime(self,d1,roundTo):
+        seconds=(d1-d1.min).seconds
+        rounding=(seconds+roundTo/2)//roundTo*roundTo
+        d2=d1+datetime.timedelta(0,rounding-seconds,-d1.microsecond)
+        if(d2>d1):
+            return d2
         else:
-            for vm,vo,dt,ms,vq,vv in unparsed_data:
-                if(dt>=fdate and dt<(tdate+datetime.timedelta(1))):
-                    data.append((str(dt.date()),str(addSecs(dt.time(),ms)),vm,vo,vq,vv))
-        if data:
-            options = {'banded_rows': 0, 'banded_columns': 1,'data': data,
-                'columns': [{'header':'Date'},
-                            {'header': 'Time'},
-                            {'header': 'Value Meantype'},
-                            {'header': 'Value Object UID '},
-                            {'header': 'Value Quality'},
-                            {'header': 'Value Value'},
-                            ]} 
-            worksheet.add_table('B3:G'+str(3+len(data)),options)
-        workbook.close()
-        return int(not data==[])
-    except PermissionError:
-        return -1
+            return d2+datetime.timedelta(seconds=roundTo)
+    def addSecs(self,tm,ms):
+        d1=datetime.datetime(1,1,1,tm.hour,tm.minute,tm.second)
+        d1+=datetime.timedelta(milliseconds=ms)
+        return d1.time()
+
+
 
 if __name__ == "__main__":
-  main(datetime.datetime(2019,5,28),datetime.datetime(2019,5,30),2,1,True,'MOSG / 33KV / H38_39 BUS SEC / BUSBAR PROT')
+  ParseData(datetime.datetime(2019,5,28),datetime.datetime(2019,5,30),2,1,False,'MOSG / 11KV / K05_T40 LV INC / MEASUREMENT / VOLTAGE VYN')
  
